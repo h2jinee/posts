6096a78971e0edb0f2bdae1f771445515f4552fd
h2jinee <wjsgmlwls97@gmail.com>
2024-09-04 18:10:55 +0900

    readme 수정

--- index.js
+++ index.js
@@ -5,20 +5,34 @@ import Parser from "rss-parser";
  * README.MD에 작성될 페이지 텍스트
  * @type {string}
  */
-let text = `# Hi there 👋
+let text = `# my posts 🗒️
 
-## 이런 환경에 익숙해요✍🏼
+### 📚 Currently Learning
 
-## 언어
+| Subject            |
+|--------------------|
+| **Spring**          |
+| **Network**         |
+| **Operating Systems** |
+| **Modern Java**     |
+| **JPA**             |
 
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
+---
+
+### 📚 Reading Now
+
+| Book                                   |
+|----------------------------------------|
+| **Clean Code**: Robert C. Martin       |
+| **자바의 신**                           |
+| **그림으로 배우는 네트워크 원리**         |
 
 ## 📕 Latest Blog Posts
 
+| No. | Title                              |
+|-----|------------------------------------|
 `;
+
 
 // rss-parser 생성
 const parser = new Parser({
@@ -31,26 +45,21 @@ const parser = new Parser({
     // 피드 목록
     const feed = await parser.parseURL('https://hoojjang.tistory.com/rss'); // 본인의 블로그 주소
     
-    text += `<ul>`;
-    
     // 최신 글의 개수와 10 중 작은 값을 사용해 반복
     for (let i = 0; i < Math.min(feed.items.length, 10); i++) {
         const { title, link } = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
-        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+        text += `| ${i + 1} | [${title}](${link}) |\n`;
     }
 
     // 만약 글이 하나도 없는 경우를 대비한 처리
     if (feed.items.length === 0) {
         console.log('게시물이 없습니다.');
-        text += `<li>게시물이 없습니다.</li>`;
+        text += `| - | 게시물이 없습니다. |\n`;
     }
 
-
-    text += `</ul>`;
-    
     // README.md 파일 생성
     writeFileSync('README.md', text, 'utf8', (e) => {
         console.log(e);
